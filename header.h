e3fed9c565e09aed5d3447344d8b64e2fbfa195b
Otávio Fagundes <62486331+tavinzix@users.noreply.github.com>
2024-12-20 10:31:55 -0300

    organização de funções

--- header.h
+++ header.h
@@ -26,38 +26,22 @@ typedef struct {
 } ListaProduto;
 
 void criaLista(ListaProduto *lp);
+
 int cadastraProduto(ListaProduto *lp, Produto p);
 int lancaNota(ListaProduto *lp, int cod);
 void exibe(ListaProduto *lp);
-
-int vendaProduto(ListaProduto *lp, Produto *p, int cod, float qtd);
-
 int consultaPorCodigo(ListaProduto *lp, int cod);
 int quantidadeDeNodos(ListaProduto *lp);
-
 int excluiProduto(ListaProduto *lp, Produto *p, int cod);
 
-
+int vendaProduto(ListaProduto *lp, Produto *p, int cod, float qtd);
 int vendaDiaria(ListaProduto *lp);
+
 int fechaCaixa(ListaProduto *lp);
 int gravaArquivo(ListaProduto *lp);
 int leArquivo(ListaProduto *lp);
 
-
-
-
-
 void barran();
 void limparTela();
 
-/*int estaVazia(ListaSE lt);
-int excluiDoInicio(ListaSE *lt, Dado *d);
-int incluiNoFim(ListaSE *lt, Dado d);
-int excluiDoFim(ListaSE *lt, Dado *d);
-int incluiDepois(ListaSE *lt, Dado d, int cod);
-int excluiNodo(ListaSE *lt, Dado *d, int cod);*/
-
-
-
 #endif
-
